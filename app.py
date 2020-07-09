3aa0028d6f3f1269729712331e8348ebf4cd5959
studio3104 <studio3104.com@gmail.com>
2020-07-09 12:56:07 +0900

    Implement a lambda function to update repos into gist

---
+++ app.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from chalice import Chalice
+from github import Github, Gist, InputFileContent
+import os
+
+
+app = Chalice(app_name='listRepositories')
+github_client = Github(os.environ['ACCESS_TOKEN'])
+ORGANIZATION_NAME = os.environ['ORGANIZATION_NAME']
+TEAM_ID = int(os.environ['TEAM_ID'])
+GIST_ID = os.environ['GIST_ID']
+
+
+@app.route('/')
+def index():
+    content: List[str] = [
+        f'[{repo.name}]({repo.url})'
+        for repo in github_client.get_organization(ORGANIZATION_NAME).get_team(TEAM_ID).get_repos()
+    ]
+
+    gist: Gist = github_client.get_gist(GIST_ID)
+    gist.edit(files={
+        'repositories.md': InputFileContent(content='  \n'.join(content))
+    })
+
+    return ''
